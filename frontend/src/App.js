db0aa84e7a39313ee972f4f1f13adab7cc8b0fd7
nayeonkinn <nayeonkinn@gmail.com>
2023-08-14 17:16:27 +0900

    Feat: 고릴라 카드 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./HomePage.scss";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <section class="cards">
+          <div class="card gorilla animated"></div>
+        </section>
+      </div>
+    );
+  }
+}
